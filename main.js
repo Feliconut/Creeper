ed85f2cf10994019f9092645c51d3b0d18a5f571
felix_liu <feliconut@outlook.com>
2020-06-15 01:04:17 +0800

    tutorial-all test

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var structTower = require('struct.tower');
 
 module.exports.loop = function () {
+    // log total energy
+    for (var name in Game.rooms) {
+        console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + ' energy');
+    }
 
-    for(var name in Game.creeps) {
+    // clean up memory for dead screeps
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+    // log harvesters
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    // Auto spawning
+    if (harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+            { memory: { role: 'harvester' } });
+    }
+
+
+    if (Game.spawns['Spawn1'].spawning) {
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            { align: 'left', opacity: 0.8 });
+    }
+
+    for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
